717cb25d08af6ad9e6041eb870f6da661b550ec8
israel <israel.j.mota@hotmai.com>
2021-09-09 16:19:46 -0300

    domains

---
+++ API/senai.inlock.webApi/senai.inlock.webApi/Domains/JogoDomain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi_.Domains
+{
+    public class JogoDomain
+    {
+        public int IdJogo { get; set; }
+
+        public EstudioDomain Estudio { get; set; }
+
+        public int IdEstudo { get; set; }
+
+        public string NomeJogo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string DataLancamento { get; set; }
+
+        public string Valor { get; set; }
+    }
+}
